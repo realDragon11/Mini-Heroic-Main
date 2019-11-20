401b35354470b18715bc46f098b38d123c3aa5f3
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2019-11-19 23:21:16 -0500

    main

---
+++ Heroic/desktop/src/com/realDragon/Heroic/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.realDragon.Heroic.desktop;
+
+import org.mini2Dx.desktop.DesktopMini2DxConfig;
+
+import com.badlogic.gdx.backends.lwjgl.DesktopMini2DxGame;
+
+import com.realDragon.Heroic.MyHeroic;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		DesktopMini2DxConfig config = new DesktopMini2DxConfig(MyHeroic.GAME_IDENTIFIER);
+		config.vSyncEnabled = true;
+		new DesktopMini2DxGame(new MyHeroic(), config);
+	}
+}
